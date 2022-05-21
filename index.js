afdbf959e8ea0f9980ffaeea736312b8dd31611a
Aryan Babber <86878236+AryanBabber@users.noreply.github.com>
2022-05-22 01:23:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 
 let password1 = document.getElementById('password1')
+const funFact = document.getElementById('fun-fact')
+let funFacts = [
+    'A 16 digit password can take 600000+ years to crack',
+    'Many passwords are easy to crack as they are commonly used by the people. Some of them are, \'12345678\', \'qwerty12\' and \'Password\'',
+    'Data breaches in major companies like Facebook and many VPN companies leaked many passwords of the users in the dark web'
+]
+
+let random = Math.floor(Math.random() * funFacts.length)
+console.log(random)
+
+// console.log(funFacts[1])
 
 function genPass(){
     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -17,3 +28,5 @@ function copyPass(){
     copyText.setSelectionRange(0, 999);
     navigator.clipboard.writeText(copyText.value);
 }
+
+funFact.textContent += funFacts[random]
